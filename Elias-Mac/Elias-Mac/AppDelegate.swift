b218b352cb6454a7041de7ba1a36db24eb92c736
ViciousPotato <Zhou.Honglin@Gmail.com>
2015-03-31 07:22:10 +0800

    Initial commit

---
+++ Elias-Mac/Elias-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Elias-Mac
+//
+//  Created by zhou on 29/3/15.
+//  Copyright (c) 2015 zhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
